8302fbcc292d12355755a47900dd95678a5bc872
Tyson Gern <tgern@pivotal.io>
2017-07-26 12:52:04 -0600

    security solution

--- src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
@@ -1,5 +1,6 @@
 package test.pivotal.pal.trackerapi;
 
+import com.jayway.jsonpath.DocumentContext;
 import io.pivotal.pal.tracker.PalTrackerApplication;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,14 +9,17 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static com.jayway.jsonpath.JsonPath.parse;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = PalTrackerApplication.class, webEnvironment = RANDOM_PORT)
-public class WelcomeApiTest {
+public class HealthApiTest {
 
     @LocalServerPort
     private String port;
@@ -31,8 +35,16 @@ public class WelcomeApiTest {
     }
 
     @Test
-    public void exampleTest() {
-        String body = this.restTemplate.getForObject("/", String.class);
-        assertThat(body).isEqualTo("Hello from test");
+    public void healthTest() {
+        ResponseEntity<String> response = this.restTemplate.getForEntity("/health", String.class);
+
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext healthJson = parse(response.getBody());
+
+        assertThat(healthJson.read("$.status", String.class)).isEqualTo("UP");
+        assertThat(healthJson.read("$.db.status", String.class)).isEqualTo("UP");
+        assertThat(healthJson.read("$.diskSpace.status", String.class)).isEqualTo("UP");
     }
 }
