236394634aa6242458aaf8dbdee64ec349c4b004
bo yu <Mac@bos-MacBook-Pro.local>
2018-02-26 12:20:30 -0700

    add security

--- src/test/java/test/pivotal/pal/trackerapi/SecurityApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/SecurityApiTest.java
@@ -30,8 +30,8 @@ public class SecurityApiTest {
     @Before
     public void setUp() throws Exception {
         RestTemplateBuilder builder = new RestTemplateBuilder()
-            .rootUri("http://localhost:" + port)
-            .basicAuthorization("user", "password");
+                .rootUri("http://localhost:" + port)
+                .basicAuthorization("user", "password");
 
         authorizedRestTemplate = new TestRestTemplate(builder);
     }
